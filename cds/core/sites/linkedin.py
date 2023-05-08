df78a0cbee710ca45db5c85f28f2844426c287ab
JoeCr117 <JoeCr117@gmail.com>
2023-05-07 21:17:59 -0700

    submitting form data part1

--- cds/core/sites/linkedin.py
+++ cds/core/sites/linkedin.py
@@ -1,8 +1,10 @@
-from ._site import _Site
-
-import time
+from time import sleep
 from bs4 import BeautifulSoup
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import ElementNotInteractableException
+
+from ._site import _Site
 
 __all__ = ["LinkedIn"]
 
@@ -39,9 +41,23 @@ class LinkedIn(_Site):
     def scrape(self, driver):
         driver.get(self.address)
         count = 0
+        sleep_time = 1
+        jitter_distance = 250
         #This is the selection of job/location/salary/company/etc. section
         #This will find all the interactable elements and 'select them'/'type in them'
         #   as configured in the intialization of the object.
+        if self.search is not None:
+            #sleep(sleep_time)
+            driver.find_element(By.XPATH,"//input[@id='job-search-bar-keywords']").send_keys(f"{self.search}")
+            #sleep(sleep_time)         
+        if self.location_a is not None:
+            #sleep(sleep_time)
+            driver.find_element(By.XPATH,"//input[@id='job-search-bar-location']").clear()
+            #sleep(sleep_time)
+            driver.find_element(By.XPATH,"//input[@id='job-search-bar-location']").send_keys(f"{self.location_a}")
+            #sleep(sleep_time)
+        driver.find_element(By.XPATH, "//button[@data-tracking-control-name='public_jobs_jobs-search-bar_base-search-bar-search-submit']").click()
+        sleep(sleep_time)
 
 
 
@@ -50,16 +66,26 @@ class LinkedIn(_Site):
 
         #This is the scroll down and 'See more jobs' button press section
         #This will loop till all the target link seen limit is reached
+        html = driver.find_element(By.TAG_NAME, 'html')
         while self.count>count:
-            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            html.send_keys(Keys.END)
+            for _ in range(jitter_distance):
+                driver.execute_script("window.scrollBy(0,-2)")
+            for _ in range(jitter_distance*2):
+                driver.execute_script("window.scrollBy(0,1)")
+            sleep(sleep_time*1.33)
             soup = BeautifulSoup(driver.page_source, "html.parser")
             a = soup.find("ul", {"class": "jobs-search__results-list"}) \
                     .find_all("a", {"class": "base-card__full-link"}, href=True)
             count = len(a)
-            time.sleep(.75)
-            curr_pos = driver.execute_script('return window.pageYOffset + window.innerHeight')
+            curr_pos = driver.execute_script('return window.scrollY + window.innerHeight')
             page_height = driver.execute_script('return document.body.scrollHeight')
             if(curr_pos == page_height):
-                driver.find_element(By.XPATH, "//button[normalize-space()='See more jobs']").click()
+                try:
+                    driver.find_element(By.XPATH, "//button[normalize-space()='See more jobs']").click()
+                    sleep(sleep_time)
+                except ElementNotInteractableException as eni:
+                    #Log this I guess
+                    del(eni)
 
         return [link['href'] for link in a], "Log Empty...for now!"
